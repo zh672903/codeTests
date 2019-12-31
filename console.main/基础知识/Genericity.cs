5110dbcd0fea6123d72c3ed8b3e374bc2734045b
PengLiang Wang <wangpengliang815@163.com>
2020-01-01 01:49:15 +0800

    2020年第一个commit

---
+++ console.main/基础知识/Genericity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace console.main.基础知识
+{
+    /// <summary>
+    /// 泛型
+    /// </summary>
+    class Genericity
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
